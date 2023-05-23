dbc14ebe924be8d85a45581d20ca09ddc7ed1c4c
This-is-Luke <lluminous.210@gmail.com>
2023-05-23 15:31:01 +0200

    task 2 update

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,8 @@
-// print hello world
+// add prompt to ask for name
+// add console.log to print out name
+// add console.log to print out "Git is Awesome!"
+
+let name = prompt("What is your name?");
+console.log("Hello " + name);
+//previous commit below
 console.log("Git is Awesome!");
